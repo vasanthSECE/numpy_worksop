7a36871bfa548c3ae4b3466dc84a0623a9bfd591
vasanthSECE <155640162+vasanthSECE@users.noreply.github.com>
2024-05-22 10:11:55 +0530

    Update question4.py

--- question4.py
+++ question4.py
@@ -1,1 +1,9 @@
 #write a program to find the sum of digits of a given number'
+def sum_of_digits(n):
+    sum_digits = 0
+    while n > 0:
+        sum_digits += n % 10
+        n = n // 10
+    return sum_digits
+number = 12345
+print("Sum of digits:", sum_of_digits(number))  
